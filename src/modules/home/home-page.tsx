21d952da1c41a3b62ab46ff4a12ca5b28f5cb683
I Putu Saputrayana <ikhyan7@gmail.com>
2025-02-18 09:11:51 +0800

    fix: fix background

--- src/modules/home/home-page.tsx
+++ src/modules/home/home-page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { AuthForm } from "./components/auth-form";
-import { PatternMain } from "@/components/svgs/pattern-main";
 
 export function HomePage() {
   return (
-    <section className="relative">
+    <section
+      className="relative bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: "url('/pattern-main.svg')" }}
+    >
       <div className="max-w-md mx-auto w-full py-28 px-6 relative z-10">
         <h1 className="text-6xl font-medium text-center">Sign In</h1>
         <h3 className="text-center text-lg text-app-text-secondary font-medium mt-6">
@@ -12,9 +14,6 @@ export function HomePage() {
         </h3>
 
         <AuthForm />
-      </div>
-      <div className="absolute inset-0 -z-10 overflow-hidden">
-        <PatternMain className="w-full h-full" />
       </div>
     </section>
   );
